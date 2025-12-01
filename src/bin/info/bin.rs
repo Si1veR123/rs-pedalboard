c62c4637a66c869ebf0fe9c0f2b4893d4e8cba5d
Si1veR1123 <connorgeorgeem@aol.com>
2025-12-01 18:42:50 +0000

    move info construction from binary to library

--- src/bin/info/bin.rs
+++ src/bin/info/bin.rs
@@ -1,75 +1,9 @@
-use std::collections::HashMap;
-
-use strum::IntoEnumIterator;
-
 use serde_json;
 
-use rs_pedalboard::pedals::{PedalDiscriminants, PedalParameter, PedalParameterValue, PedalTrait, info::{PedalInfo, ParameterInfo, Info}};
+use rs_pedalboard::pedals::info::Info;
 
 fn main() {
-    let mut info = Info { pedals: Vec::new() };
-
-    for pedal_type in PedalDiscriminants::iter() {
-        let mut init_pedal = pedal_type.new_pedal();
-
-        let mut pedal_info = PedalInfo {
-            name: pedal_type.display_name().to_string(),
-            parameters: HashMap::new()
-        };
-
-        let params: Vec<(String, PedalParameter)> = init_pedal.get_parameters_mut()
-            .into_iter()
-            .map(|(n, p)| (n.clone(), p.clone()))
-            .collect();
-
-        for (name, param) in &params {
-            let param_name = name.to_owned();
-            
-            match param.value.clone() {
-                PedalParameterValue::Bool(_) => {
-                    pedal_info.parameters.insert(
-                        param_name,
-                        ParameterInfo::Options(vec![
-                            PedalParameterValue::Bool(false),
-                            PedalParameterValue::Bool(true)
-                        ])
-                    );
-                },
-                PedalParameterValue::Int(_) => {
-                    pedal_info.parameters.insert(
-                        param_name,
-                        ParameterInfo::Numerical(param.clone())
-                    );
-                },
-                PedalParameterValue::Float(_) => {
-                    pedal_info.parameters.insert(
-                        param_name,
-                        ParameterInfo::Numerical(param.clone())
-                    );
-                },
-                PedalParameterValue::String(_) => {
-                    pedal_info.parameters.insert(
-                        param_name,
-                        ParameterInfo::Options(
-                            init_pedal.get_string_values(&name)
-                                .expect("PedalParameterValue::String must have discrete values")
-                                .into_iter()
-                                .map(|s| PedalParameterValue::String(s))
-                                .collect()
-                        )
-                    );
-                },
-                PedalParameterValue::Oscillator(o) => {
-                    pedal_info.parameters.insert(
-                        param_name,
-                        ParameterInfo::Oscillator(o)
-                    );
-                },
-            }
-        }
-
-        info.pedals.push(pedal_info);
-    }
+    let info = Info::pedal_defaults();
 
     let json_str = serde_json::to_string_pretty(&info).expect("Failed to serialize info to JSON");
 
--- src/pedals/info.rs
+++ src/pedals/info.rs
@@ -1,4 +1,6 @@
 use serde::{Serialize, Deserialize};
+use strum::IntoEnumIterator;
+use crate::pedals::{PedalDiscriminants, PedalTrait};
 use super::{PedalParameter, PedalParameterValue, Oscillator};
 use std::collections::HashMap;
 
@@ -18,4 +20,74 @@ pub struct PedalInfo {
 #[derive(Serialize, Deserialize)]
 pub struct Info {
     pub pedals: Vec<PedalInfo>
+}
+
+impl Info {
+    pub fn pedal_defaults() -> Self {
+        let mut info = Info { pedals: Vec::new() };
+
+        for pedal_type in PedalDiscriminants::iter() {
+            let mut init_pedal = pedal_type.new_pedal();
+
+            let mut pedal_info = PedalInfo {
+                name: pedal_type.display_name().to_string(),
+                parameters: HashMap::new()
+            };
+
+            let params: Vec<(String, PedalParameter)> = init_pedal.get_parameters_mut()
+                .into_iter()
+                .map(|(n, p)| (n.clone(), p.clone()))
+                .collect();
+
+            for (name, param) in &params {
+                let param_name = name.to_owned();
+                
+                match param.value.clone() {
+                    PedalParameterValue::Bool(_) => {
+                        pedal_info.parameters.insert(
+                            param_name,
+                            ParameterInfo::Options(vec![
+                                PedalParameterValue::Bool(false),
+                                PedalParameterValue::Bool(true)
+                            ])
+                        );
+                    },
+                    PedalParameterValue::Int(_) => {
+                        pedal_info.parameters.insert(
+                            param_name,
+                            ParameterInfo::Numerical(param.clone())
+                        );
+                    },
+                    PedalParameterValue::Float(_) => {
+                        pedal_info.parameters.insert(
+                            param_name,
+                            ParameterInfo::Numerical(param.clone())
+                        );
+                    },
+                    PedalParameterValue::String(_) => {
+                        pedal_info.parameters.insert(
+                            param_name,
+                            ParameterInfo::Options(
+                                init_pedal.get_string_values(&name)
+                                    .expect("PedalParameterValue::String must have discrete values")
+                                    .into_iter()
+                                    .map(|s| PedalParameterValue::String(s))
+                                    .collect()
+                            )
+                        );
+                    },
+                    PedalParameterValue::Oscillator(o) => {
+                        pedal_info.parameters.insert(
+                            param_name,
+                            ParameterInfo::Oscillator(o)
+                        );
+                    },
+                }
+            }
+
+            info.pedals.push(pedal_info);
+        }
+
+        info
+    }
 }
