e1495ada505e1d362d234f93debe744e15ac7936
Si1veR123 <connorgeorgeem@aol.com>
2026-09-10 20:06:45 +0100

    only rebuild pitch shift engine when block size changes

--- src/pedals/pitch_shift.rs
+++ src/pedals/pitch_shift.rs
@@ -271,12 +271,22 @@ impl PedalTrait for PitchShift {
                         ));
                     }
                 } else {
+                    let rebuild = name == "Block Size";
                     parameter.value = value;
                     if let Some((_eq, sample_rate)) = &self.eq {
-                        self.signalsmith_stretch = Some(Self::stretch_from_parameters(
-                            &self.parameters,
-                            *sample_rate as f32,
-                        ));
+                        if rebuild {
+                            self.signalsmith_stretch = Some(Self::stretch_from_parameters(
+                                &self.parameters,
+                                *sample_rate as f32,
+                            ));
+                        } else if name == "Semitones" || name == "Tonality Limit" {
+                            let semitones = self.parameters.get("Semitones").unwrap().value.as_int().unwrap() as f32;
+                            let tonality_limit = self.parameters.get("Tonality Limit").unwrap().value.as_float().unwrap() / *sample_rate as f32;
+                            if let Some(stretch) = &mut self.signalsmith_stretch {
+                                stretch
+                                    .set_transpose_factor_semitones(semitones, Some(tonality_limit));
+                            }
+                        }
                     }
                 }
             }
