9c2aac4fc2de294068b35deb052771cdeef3a80f
Si1veR123 <connorgeorgeem@aol.com>
2026-09-10 19:45:35 +0100

    wrap oscillator phase with rem_euclid

--- src/dsp_algorithms/oscillator.rs
+++ src/dsp_algorithms/oscillator.rs
@@ -139,12 +139,8 @@ impl Iterator for Sine {
 
     /// Never returns None
     fn next(&mut self) -> Option<f32> {
-        if self.phase.0 > 1.0 {
-            self.phase -= 1.0;
-        }
-
         let value = ((self.phase.0 + self.phase_offset.0) * 2.0 * std::f32::consts::PI).sin();
-        self.phase += self.frequency / self.sample_rate;
+        self.phase = OrderedFloat((self.phase.0 + self.frequency.0 / self.sample_rate.0).rem_euclid(1.0));
 
         if self.squareness.0 == 0.0 {
             Some(value)
@@ -178,16 +174,12 @@ impl Iterator for Square {
 
     /// Never returns None
     fn next(&mut self) -> Option<f32> {
-        if self.phase.0 > 1.0 {
-            self.phase -= 1.0;
-        }
-
         let value = if (self.phase.0 + self.phase_offset.0) % 1.0 < 0.5 {
             1.0
         } else {
             -1.0
         };
-        self.phase += self.frequency / self.sample_rate;
+        self.phase = OrderedFloat((self.phase.0 + self.frequency.0 / self.sample_rate.0).rem_euclid(1.0));
 
         Some(value)
     }
@@ -217,12 +209,8 @@ impl Iterator for Sawtooth {
 
     /// Never returns None
     fn next(&mut self) -> Option<f32> {
-        if self.phase.0 > 1.0 {
-            self.phase -= 1.0;
-        }
-
         let value = 2.0 * ((self.phase.0 + self.phase_offset.0) % 1.0) - 1.0;
-        self.phase += self.frequency / self.sample_rate;
+        self.phase = OrderedFloat((self.phase.0 + self.frequency.0 / self.sample_rate.0).rem_euclid(1.0));
 
         Some(value)
     }
@@ -252,10 +240,6 @@ impl Iterator for Triangle {
 
     /// Never returns None
     fn next(&mut self) -> Option<f32> {
-        if self.phase.0 > 1.0 {
-            self.phase -= 1.0;
-        }
-
         let phase_with_offset = (self.phase.0 + self.phase_offset.0) % 1.0;
         let value = if phase_with_offset < 0.5 {
             4.0 * phase_with_offset - 1.0
@@ -263,7 +247,7 @@ impl Iterator for Triangle {
             -4.0 * phase_with_offset + 3.0
         };
 
-        self.phase += self.frequency / self.sample_rate;
+        self.phase = OrderedFloat((self.phase.0 + self.frequency.0 / self.sample_rate.0).rem_euclid(1.0));
 
         Some(value)
     }
